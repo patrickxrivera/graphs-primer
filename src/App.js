ecbeca6094275e93341f3582e07787f7b8cad1b9
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-23 23:02:45 -0400

    Implemented v1 of updateCanvas

--- src/App.js
+++ src/App.js
@@ -2,74 +2,102 @@ import React, { Component } from 'react';
 import { Graph } from './graph';
 import './App.css';
 
-// !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 780;
+const canvasHeight = 630;
 
-/**
- * GraphView
- */
 class GraphView extends Component {
-  /**
-   * On mount
-   */
+  state = {
+    ctx: null
+  };
+
   componentDidMount() {
     this.updateCanvas();
   }
 
-  /**
-   * On state update
-   */
   componentDidUpdate() {
     this.updateCanvas();
   }
 
-  /**
-   * Render the canvas
-   */
-  updateCanvas() {
-    let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
-    // Clear it
+  initCanvas = () => {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+
+    this.setState({ ctx });
+  };
+
+  updateCanvas = async () => {
+    const { ctx } = this.state;
+
+    if (!ctx) {
+      await this.initCanvas(); // ensure setState is finished before returning
+      return;
+    }
+
+    this.clearCanvas(ctx);
+    this.initGraph(ctx);
+  };
+
+  clearCanvas = (ctx) => {
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  };
 
-    // !!! IMPLEMENT ME
-    // compute connected components
-    // draw edges
-    // draw verts
-    // draw vert values (labels)
-  }
-  
+  initGraph = (ctx) => {
+    const { graph } = this.props;
+
+    graph.randomize(5, 4, 150, 0.6);
+
+    graph.vertexes.forEach((v) => {
+      this.drawCircle(v);
+      v.edges.forEach(this.drawLine(ctx, v));
+    });
+  };
+
+  drawCircle = ({ pos, value }) => {
+    const { ctx } = this.state;
+    const { x } = pos;
+    const { y } = pos;
+    const radius = 20;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'black';
+    ctx.font = '16px Arial';
+    ctx.fillText(value, x, y, 50);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+  };
+
+  drawLine = (ctx, v) => (e) => {
+    const { destination } = e;
+
+    ctx.beginPath();
+    ctx.moveTo(v.pos.x, v.pos.y);
+    ctx.lineTo(destination.pos.x, destination.pos.y);
+    ctx.stroke();
+  };
+
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    return <canvas className="canvas" ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
 
-
-/**
- * App
- */
 class App extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    graph: new Graph()
+  };
 
-    this.state = {
-      graph: new Graph()
-    };
-
-    // !!! IMPLEMENT ME
-    // use the graph randomize() method
-  }
+  // !!! IMPLEMENT ME
+  // use the graph randomize() method
 
   render() {
     return (
       <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
+        <GraphView graph={this.state.graph} />
       </div>
     );
   }
---
+++ test/graph.test.js
@@ -0,0 +1,11 @@
+// import Graph from '../src/graph';
+//
+// describe(('Graph'), () => {
+//   let graph;
+//
+//   beforeEach(() => {
+//     graph = new Graph();
+//   })
+//
+//   it('should iterate through all the vertexes and apply a cb function', () => )
+// })
